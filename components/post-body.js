78e74bc5dce57938043f38970149452b5fcfbc16
Zac Halbert <zachalbert@gmail.com>
2023-12-18 23:16:47 -0800

    add tw typography

--- components/post-body.js
+++ components/post-body.js
@@ -1,10 +1,13 @@
-import markdownStyles from './markdown-styles.module.css'
-import { PortableText } from '@portabletext/react'
+import markdownStyles from "./markdown-styles.module.css";
+import { PortableText } from "@portabletext/react";
+import cx from "classnames";
 
 export default function PostBody({ content }) {
   return (
-    <div className={`max-w-2xl mx-auto ${markdownStyles.markdown}`}>
+    <div
+      className={cx("prose", "max-w-2xl", "mx-auto", markdownStyles.markdown)}
+    >
       <PortableText value={content} />
     </div>
-  )
+  );
 }
